28bc1dce68624ff084a1269d335606b75a1e80b3
Muhammad <muhammad.ali.72.27@gmail.com>
2020-06-07 15:27:04 -0700

    added structure to files and few Components to use

---
+++ src/Components/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const logo = (props) => {
+   return (
+       <div>
+           <h1>Where's Waldo</h1>
+       </div>
+   )
+}
+
+export default logo;
---
+++ src/Containers/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../logo.svg';
+import './App.css';
+import Logo from '../Components/Logo'
+
+function App() {
+  return (
+    <div className="App">
+      <Logo/>
+    </div>
+  );
+}
+
+
+
+export default App;
