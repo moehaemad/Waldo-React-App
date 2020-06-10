9feeacc01009e71f0d71cf4de94a9c2ee0153afd
Muhammad <muhammad.ali.72.27@gmail.com>
2020-06-09 21:51:45 -0700

    finished most of the RanString component

--- src/Components/Generator/RanString.js
+++ src/Components/Generator/RanString.js
@@ -1,22 +1,53 @@
 import React from 'react';
 import GeneratorCSS from './Generator.module.css';
 const RanString = () => {
-    const [state, setState] = React.useState({open: true});
+    const [state, setState] = React.useState({
+        open: true,
+        capital: false,
+        special: false
+    });
 
     const toggleWindow = () => {
         state.open ? setState({open: false}) : setState({open:true});
     }
 
-    const options = () => {
+    const changeValues = (e, type) => {
+        const checked = e.target.checked;
+        const prevState = state;
+        if (type === 'capital') setState({open: prevState.open, capital: checked, special: prevState.special});
+        if (type === 'special') setState({open: prevState.open, capital: prevState.capital, special: checked});
+    }
+
+    const createString = () => {
+        let abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+        const special = [`!`, `"`, `#`, `$`, `%`, `&`, `'`, `(`, `)`, `*`, `+`, `,`, `-`, `.`, `/`, `:`, `;`, `<`, `=`, `>`, `?`, `@`, `[`, `]`, `@`, `^`, `_`, `{`, `}`, `~`];
+        const ABC = abc.map(el => el.toUpperCase());
+        let toReturn = [];
+        if (state.capital) toReturn = toReturn.concat(abc, ABC);
+        if (state.special) toReturn = toReturn.concat(special);
+
         return (
-            <div className={GeneratorCSS.Options}>
-                {/* <input type="checkbox" id="something"/> */}
-                <label><input type="checkbox"/> include capital letters</label>
-                <label><input type="checkbox"/> special characters</label>
-                <label>Length: <input type="number"/></label>
+            <div>
+                <p>{toReturn}</p>
             </div>
         );
     }
+
+    const options = () => {
+
+        // const output = createString();
+
+        return (
+            <div className={GeneratorCSS.Options}>
+                {/* <input type="checkbox" id="something"/> */}
+                <label><input type="checkbox" onChange={(e) => changeValues(e, 'capital')}/> include capital letters</label>
+                <label><input type="checkbox" onChange={(e) => changeValues(e, 'special')}/> special characters</label>
+                <label>Length: <input type="number"/></label>
+                {/* {output} */}
+            </div>
+        );
+    }
+
     return (
         <div className={GeneratorCSS.Output}>
             <div className={GeneratorCSS.Title}>
@@ -26,6 +57,7 @@ const RanString = () => {
                 <button className={GeneratorCSS.Close} onClick={toggleWindow}>{state.open ? 'X' : 'O'}</button>
             </div>
             {state.open ? options() : null}
+            {createString()}
         </div>
     );
 }
